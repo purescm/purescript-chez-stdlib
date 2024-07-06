90a6e7af331be9d8d4698c56545dc2dc5f472c2b
Fabrizio Ferrai <fabrizio.ferrai@gmail.com>
2024-07-07 02:45:10 +0300

    Add simple wrapper for the threading API (#1)

---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  description = "PureScript core packages CI setup";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+
+    purescript-overlay.url = "github:thomashoneyman/purescript-overlay";
+    purescript-overlay.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" ];
+
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+      nixpkgsFor = forAllSystems (system: import nixpkgs {
+        inherit system;
+        overlays = builtins.attrValues self.overlays;
+      });
+    in
+    {
+      overlays = {
+        purescript = inputs.purescript-overlay.overlays.default;
+      };
+
+      devShells = forAllSystems (system:
+        let pkgs = nixpkgsFor.${system};
+            chez = pkgs.chez.overrideAttrs (final: prev: {
+              postFixup = if pkgs.stdenv.isDarwin then ''
+                install_name_tool -add_rpath ${pkgs.pcre2.out}/lib $out/bin/scheme
+              ''
+              else ''
+                patchelf $out/bin/scheme --add-rpath ${pkgs.pcre2.out}/lib
+              '';
+            });
+        in {
+          default = pkgs.mkShell {
+            name = "purescm";
+            packages = with pkgs; [
+              chez
+            ];
+          };
+        });
+
+      formatter = forAllSystems (system: nixpkgsFor.${system}.nixpkgs-fmt);
+    };
+}
---
+++ test/Test/Main.purs
@@ -0,0 +1,35 @@
+module Test.Chez.Main where
+
+import Prelude
+
+import Chez.Thread as Thread
+import Data.Array as Array
+import Data.Time.Duration (Milliseconds(..))
+import Data.Traversable (for, for_)
+import Effect (Effect)
+import Effect.Ref as Ref
+import Effect.Unsafe (unsafePerformEffect)
+import Test.Assert (assertEqual)
+
+main :: Effect Unit
+main = do
+  counter <- Ref.new 0
+
+  let
+    modifyFn :: Int -> Int
+    modifyFn x = unsafePerformEffect do
+      Thread.sleep (Milliseconds 10.0)
+      pure (x + 1)
+
+    threadFn _threadId = do
+      for_ (Array.range 0 9) \_ -> do
+        newCounter <- Ref.modify modifyFn counter
+        -- log $ "Thread " <> show (Thread.threadIdToInt threadId) <> ": new counter is " <> show newCounter
+        pure newCounter
+
+  threads <- for (Array.range 0 9) \_ -> Thread.fork threadFn
+
+  for_ threads Thread.join
+
+  finalCount <- Ref.read counter
+  assertEqual { actual: finalCount, expected: 100 }
